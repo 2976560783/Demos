63aae82ebcba48b6f308189d6fa99231ff932fbd
jwhuang59 <33829699+jwhuang59@users.noreply.github.com>
2017-12-21 19:23:28 +0800

    Add files via upload

---
+++ msg/Messages.sql
@@ -0,0 +1,65 @@
+﻿
+
+-- 建表语句 Messages
+
+CREATE TABLE `Messages` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) DEFAULT NULL,
+  `mobile` varchar(11) DEFAULT NULL,
+  `city` varchar(32) DEFAULT NULL,
+  `type` varchar(32) DEFAULT NULL,
+  `date` varchar(32) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=23 DEFAULT CHARSET=gbk
+
+
+
+
+-- 表中的数据 
+
+
+
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('22','上的','16935861486','深圳','联通','2017-8-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('21','大姐','15939549869','广州','移动','2017-6-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('20','李玉','15938648624','上海','电信','2017-6-4');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('19','人人','19924862866','北京','移动','2017-5-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('18','大户','15938968969','深圳','电信','2017-8-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('17','返回','15935865966','广州','移动','2017-9-6');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('16','偶尔','15938954959','北京','联通','2017-6-8');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('15','点后','15935851625','深圳','电信','2017-9-5');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('14','名单','15938948669','广州','移动','2017-7-8');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('13','区域','15935862495','上海','移动','2017-2-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('12','黄五','15939628964','北京','电信','2017-9-3');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('11','周达','13968954861','上海','电信','2017-5-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('10','周都','15932486269','上海','联通','2017-10-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('9','李到','15935895485','北京','移动','2017-9-8');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('8','张也','17593896325','深圳','移动','2017-10-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('7','周姐','17593289636','北京','联通','2017-4-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('6','张宇','15935896426','北京','联通','2017-9-6');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('5','黄已','15963896256','广州','移动','2017-12-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('4','某某','15932865956','上海','联通','2017-5-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('3','王五','17593586296','广州','联通','2017-6-9');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('2','李四','19263586745','北京','移动','2017-5-8');
+insert into `Messages`(`id`,`name`,`mobile`,`city`,`type`,`date`)
+values('1','张三','13031269256','上海','移动','2017-6-8');
---
+++ msg/page.php
@@ -0,0 +1,22 @@
+﻿<?php  
+$conn=new Mysqli(host,username,password,dbname);  
+if(!$conn){  
+    echo "数据库连接错误!";exit;  
+}  
+$page=$_POST['page']; //获取前台传过来的页码  
+$pageSize=6;  //设置每页显示的条数  
+$start=($page-1)*6; //从第几条开始取记录  
+mysqli_query($conn,"set names 'utf8'");  
+$result=$conn->query("select * from Messages limit {$start},{$pageSize}");  
+
+$count=$conn->query("select id from Messages")->num_rows;//记录总条数
+$totalPage=ceil($count/$pageSize);//页码数
+
+
+while($row=mysqli_fetch_array($result)){  
+    $datas[] = array("id"=>$row['id'],"name"=>$row['name'],"mobile"=>$row['mobile'],"city"=>$row['city'],"type"=>$row['type'],"date"=>$row['date'],"count"=>$count,"totalPage"=>$totalPage);
+}  
+
+echo json_encode($datas);
+mysqli_close($conn);
+?>
